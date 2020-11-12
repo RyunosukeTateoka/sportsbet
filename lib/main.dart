714552029079d9cfadd0a9e0e440ec898bf70723
Ryunosuke Tateoka <shabadabastyle@gmail.com>
2020-11-12 20:31:59 +0900

    Color変数の指定を全量他ファイル（color_configuration.dart）に分離

--- lib/main.dart
+++ lib/main.dart
@@ -5,21 +5,7 @@ import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:provider/provider.dart';
-
-const Color whiteColor = Color.fromARGB(255, 240, 240, 240);
-const Color textOnWhiteButtonColor = Color.fromARGB(255, 56, 61, 66);
-const Color tabsColor = Color.fromARGB(230, 5, 10, 16);
-const Color loginSignUpTabScreenColor = Color.fromARGB(230, 5, 10, 22);
-const Color focusedColor = Color.fromARGB(255, 69, 122, 229);
-const Color linkedColor = Color.fromARGB(255, 69, 122, 229);
-const Color hintTextColor = Color.fromARGB(60, 199, 199, 199);
-const Color completedColor = Color.fromARGB(255, 149, 195, 151);
-const Color facebookColor = Color.fromARGB(255, 59, 89, 152);
-const Color googleColor = Color.fromARGB(255, 219, 68, 55);
-const Color inactiveButtonColor = Color.fromARGB(255, 48, 58, 72);
-const Color inactiveButtonTextColor = Color.fromARGB(255, 74, 80, 86);
-const Color accentColorStart = Color.fromARGB(255, 214, 114, 247);
-const Color accentColorEnd = Color.fromARGB(255, 250, 126, 78);
+import 'package:sportsbet/assets/color_configuration.dart';
 
 const _tab = <Tab> [
   Tab( text:'ログイン'),
@@ -39,8 +25,7 @@ class MyApp extends StatelessWidget {
       home: InitLoginSignUpScreen(),
       routes: <String, WidgetBuilder>{
         '/init': (BuildContext context) => new InitLoginSignUpScreen(),
-        '/loginSignUpFormScreen': (
-            BuildContext context) => new LoginSignUpFormScreen()
+        '/loginSignUpFormScreen': (BuildContext context) => new LoginSignUpFormScreen()
       },
     );
   }
